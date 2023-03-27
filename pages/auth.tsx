b26ffc519a95051f635c016a448c4f40110793a2
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-27 19:57:56 -0400

    fully setup login functionality with page redirect to home

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useCallback } from 'react'
 import Input from '../components/Input';
 import axios from 'axios';
-
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 
 const Auth = () => {
+
+    const router = useRouter();
+
+
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -15,6 +20,22 @@ const Auth = () => {
       setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
 
+
+    const login = useCallback(async () => {
+        try {
+          await signIn('credentials', {
+            email,
+            password,
+            redirect: false,
+            callbackUrl: '/'
+          });
+    
+          router.push('/');
+        } catch (error) {
+          console.log(error);
+        }
+      }, [email, password, router]);
+
     const register = useCallback(async () => {
     try {
       await axios.post('/api/register', {
@@ -22,11 +43,11 @@ const Auth = () => {
         name,
         password
       });
-
+      login();
     } catch (error) {
         console.log(error);
     }
-  }, [email, name, password]);
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -64,7 +85,7 @@ const Auth = () => {
                             onChange={(e: any) => setPassword(e.target.value)}
                         />
 
-                        <button onClick={register}  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register}  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
 
