b75f1676bd855300c4f7e2a2f6c920a47fdec7b6
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-29 02:56:04 -0400

    fully implemented favorites functionality. Added the ability to add a movie to a list of favorites for a user.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { BsFillPlayFill } from 'react-icons/bs';
 
 import { BsChevronDown, BsSearch, BsBell } from 'react-icons/bs';
+import FavoriteButton from './FavoriteButton';
 
 
 
@@ -73,9 +74,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             <div  className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
               <BsFillPlayFill className="text-black w-4 lg:w-6" />
             </div>
-            <div  className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+            <FavoriteButton movieId={data?.id} />
+            {/* <div  className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
               <BsChevronDown className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
-            </div>
+            </div> */}
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar';
 
 import useCurrentUser from '../hooks/useCurrentUser';
 import useMovieList from '../hooks/useMovieList';
+import useFavorites from '../hooks/useFavorites';
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -28,14 +29,16 @@ export async function getServerSideProps(context: NextPageContext) {
 const Home = () => {
   const {data: user} = useCurrentUser();
   const { data: movies = [] } = useMovieList();
+  const {data: favorites = []} = useFavorites();
 
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
+      <MovieList title="My Favorites" data={favorites} />
         <MovieList title="Trending Now" data={movies} />
-        {/* <MovieList title="My List" data={favorites} /> */}
+
       </div>
     </>
   )
